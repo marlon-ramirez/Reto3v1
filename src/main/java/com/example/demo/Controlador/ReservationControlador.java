e98f7e2fd91b0ac74c2f537483b971850ccdbba1
marlon <marlon.ramirez.mt@usa.edu.co>
2022-10-15 22:50:45 -0500

    second commit

--- src/main/java/com/example/demo/Controlador/ReservationControlador.java
+++ src/main/java/com/example/demo/Controlador/ReservationControlador.java
@@ -5,12 +5,13 @@
 package com.example.demo.Controlador;
 
 import com.example.demo.Modelo.Reservation;
+import com.example.demo.Repositorio.CountClient;
 import com.example.demo.Servicio.ReservationServicio;
+import com.example.demo.Servicio.Status;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +28,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/Reservation")
-@CrossOrigin(origins = "*")
 
 public class ReservationControlador {
     @Autowired
@@ -60,4 +60,20 @@ public class ReservationControlador {
         return reservationService.deleteReservation(reservationId);
     }
     
+    ///////////RETO 5 ///////////////////
+    
+    @GetMapping("/report-clients")
+    public List<CountClient> getReservationsReportClient(){
+        return reservationService.getTopClients();
+    }
+    
+    @GetMapping("/report-dates/{dateOne}/{dateTwo}")
+    public List<Reservation> getReservationsReportReportDates(@PathVariable("dateOne") String dateOne,@PathVariable("dateTwo") String dateTwo){
+        return reservationService.informePeriodoTiempoReservas(dateOne,dateTwo);
+    }
+    
+    @GetMapping("/report-status")
+    public Status getReservationsStatusReport(){
+        return reservationService.getReservationStatusReport();
+    }
 }
